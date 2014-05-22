a79999b77f95215ee1844ee8e098bce56c97c42d
Manuel Dudda <dudda@redpeppix.de>
2014-05-22 22:37:13 +0200

    default index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def index
     o = OpenLigaDbDataWrapper.new
     @matchdays = o.brasil_matches o.brasil_rounds.first
-    @current_index = @matchdays.index { |m| m.last.first.first.eql?(params[:uri]) }
+    @current_index = @matchdays.index { |m| m.last.first.first.eql?(params[:uri]) } || 0
     @matchday = @matchdays[@current_index]
     render text: 'not avaibable' unless @current_index
   end
