d6d882c3f1eb5a0a73c13ed1ac8b3d6b80e6652e
Manuel Dudda <dudda@redpeppix.de>
2014-05-22 17:30:20 +0200

    pages controller swipe jquery mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
 
-  # GET /pages
-  # GET /pages.json
   def index
-  end
-
-  def show_matchday
-
+    prefix = 'http://localhost:3000/matchdays/'
+    @matchdays = (1...64).map { |i| "#{prefix}#{i}" }
+    @matchday = params[:uri]
+    @current_index = @matchdays.index(@matchday)
+    render text: 'not avaibable' unless @current_index
   end
 
 end
