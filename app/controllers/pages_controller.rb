f01294f8f06934597a1cdd24dfd2bb07c10eb948
Manuel Dudda <dudda@redpeppix.de>
2014-05-22 17:50:43 +0200

    minor fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def index
     prefix = 'http://localhost:3000/matchdays/'
-    @matchdays = (1...64).map { |i| "#{prefix}#{i}" }
+    @matchdays = (1..64).map { |i| "#{prefix}#{i}" }
     @matchday = params[:uri]
     @current_index = @matchdays.index(@matchday)
     render text: 'not avaibable' unless @current_index
