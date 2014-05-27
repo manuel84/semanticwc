02ceb6e3d5032e29a41e2ee3ea59ee41248b21ca
Manuel Dudda <dudda@redpeppix.de>
2014-05-27 22:52:14 +0200

    player page team page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,16 @@ class PagesController < ApplicationController
   def index
     o = OpenLigaDbDataWrapper.new
     @matchdays = o.brasil_matches o.brasil_rounds.first
-    @current_index = @matchdays.index { |m| m.last.first.first.eql?(params[:uri]) } || 0
-    @matchday = @matchdays[@current_index]
-    render text: 'not avaibable' unless @current_index
+    if params[:uri] && params[:uri].starts_with?('team_')
+      render :team
+    elsif params[:uri] && params[:uri].starts_with?('player_')
+      render :player
+    else
+      @current_index = @matchdays.index { |m| m.last.first.first.eql?(params[:uri]) } || 0
+      @matchday = @matchdays[@current_index]
+      render text: 'not avaibable' unless @current_index
+    end
+
   end
 
 end
