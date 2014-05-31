5f4d6298150404f0ced2d01d56b21a6c960f31c9
Manuel Dudda <dudda@redpeppix.de>
2014-05-31 11:09:59 +0200

    ttl: customized concrete World Cup 2014 to a MultiStageCompetition

--- app/helpers/rdf_helper.rb
+++ app/helpers/rdf_helper.rb
@@ -2,8 +2,8 @@ module RdfHelper
   RDF_TTL_FILE = (Rails.root.join 'doc', 'example.ttl').to_s
   QUERYABLE = RDF::Repository.load(RDF_TTL_FILE)
 
-  def get_divisional_competitions
-    sparql = SPARQL.parse("SELECT * WHERE { ?s <#{PREFIX::RDF}type> <#{PREFIX::BBCSPORT}DivisionalCompetition> }")
+  def get_multi_stage_competitions
+    sparql = SPARQL.parse("SELECT * WHERE { ?s <#{PREFIX::RDF}type> <#{PREFIX::BBCSPORT}MultiStageCompetition> }")
     solutions = QUERYABLE.query(sparql)
   end
 end
--- spec/helpers/rdf_helper_spec.rb
+++ spec/helpers/rdf_helper_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 describe RdfHelper do
   describe "#ttl file" do
-    it "Fußball-Weltmeisterschaft_2014 has as DivisionalCompetition" do
-      divisional_competitions = helper.get_divisional_competitions
+    it "Fußball-Weltmeisterschaft_2014 has as MultiStageCompetition" do
+      divisional_competitions = helper.get_multi_stage_competitions
       divisional_competitions.filter { |solution| solution.s.eql?(RDF::URI.new "http://de.dbpedia.org/page/Fu%C3%9Fball-Weltmeisterschaft_2014") }
       divisional_competitions.should_not be_empty
     end
