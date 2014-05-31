4977cdbce37d477516300566f17d26a70b0ce86d
Manuel Dudda <dudda@redpeppix.de>
2014-05-31 12:15:27 +0200

    extended the example ttl for world cup 2014 until Group A matches

--- spec/helpers/rdf_helper_spec.rb
+++ spec/helpers/rdf_helper_spec.rb
@@ -3,7 +3,7 @@ describe RdfHelper do
   describe "#ttl file" do
     it "Fußball-Weltmeisterschaft_2014 has as MultiStageCompetition" do
       divisional_competitions = helper.get_multi_stage_competitions
-      divisional_competitions.filter { |solution| solution.s.eql?(RDF::URI.new "http://de.dbpedia.org/page/Fu%C3%9Fball-Weltmeisterschaft_2014") }
+      divisional_competitions.filter { |solution| solution.s.eql?(RDF::URI.new "http://de.dbpedia.org/page/Fußball-Weltmeisterschaft_2014") }
       divisional_competitions.should_not be_empty
     end
   end
