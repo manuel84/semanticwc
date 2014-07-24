faa50d862c30f574be629593cf7a1210e4977d88
Manuel Dudda <manueldudda@redpeppix.de>
2014-07-24 18:48:49 +0200

    trainer view draft

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,8 @@ class TeamsController < ApplicationController
   def index
     @team = get_team(params[:uri])
     @title = @team.label
-    @players = get_players(params[:uri])
+    @players = get_players_for_team(params[:uri])
+    @trainer = get_trainer_for_team(params[:uri])
   end
 
 end
