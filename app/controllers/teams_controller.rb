9857fa8d70c6e2c9b1ddd78f22fcd76d8b7c2c2f
Manuel Dudda <manueldudda@redpeppix.de>
2014-08-05 16:58:57 +0200

    show matches at matches#index

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,6 +4,7 @@ class TeamsController < ApplicationController
     @team = get_team(params[:uri]) #includes trainer info
     @title = @team.label
     @players = get_players_for_team(params[:uri])
+    @matches, filter_type = get_matches(params[:uri])
     #@trainer = get_trainer(@team.coach_uri)
   end
 
