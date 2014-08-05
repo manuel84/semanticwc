0d914ab7b52a2f9087fece9c535a86d18d69b8b7
Manuel Dudda <manueldudda@redpeppix.de>
2014-08-05 18:17:42 +0200

    default match the last

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,7 +4,7 @@ class MatchesController < ApplicationController
     @filter_value = params[:filter_uri] || ''
 
     @matches, @filter_type = get_matches(@filter_value)
-    @current_index = @matches.index { |matchday| matchday.uri.to_s.eql?(params[:uri]) } || -1
+    @current_index = @matches.index { |matchday| matchday.uri.to_s.eql?(params[:uri]) } || @matches.count-1
     @match = @matches[@current_index]
     @goals = get_goals(@match.uri)
     @home = get_team(@match.homeCompetitor_uri)
